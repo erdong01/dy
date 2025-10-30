2667f7b2257e0879a9fbc5a38dc80c18009f947d
erdong01 <erdong88155745@outlook.com>
2025-10-30 15:45:23 +0800

    fix

--- scripts/generate_sitemap.go
+++ scripts/generate_sitemap.go
@@ -20,7 +20,6 @@ var headerLines = []string{
 	`  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9`,
 	`            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`,
 	``,
-	`  <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->`,
 }
 
 func main() {
